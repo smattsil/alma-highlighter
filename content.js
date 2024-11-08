a7a0e3c9d584ba40f22add4b1fd7d25dd6d7ca11
Steven Silitonga <steven.matt.sil@gmail.com>
2024-11-08 18:39:56 +0700

    first comm.

---
+++ content.js
@@ -0,0 +1,78 @@
+
+function extractNumber(str) {
+    const match = str.match(/\((\d+)%\)/); // Regex to match a number inside parentheses, followed by a percentage sign
+    return match ? parseInt(match[1]) : null; // Return the matched number, or null if no match
+}
+
+function highlightOverview() {
+
+    const gradeElements = document.querySelectorAll(".class");
+
+    gradeElements.forEach(element => {
+
+        element.style.backgroundColor = "lightgray";
+
+        const percentElement = element.querySelector(".percent");
+
+        if (percentElement) {
+            const grade = extractNumber(percentElement.textContent);
+            if (grade !== null) {
+
+                if (grade > 93) {
+                    element.style.backgroundColor = "#98FB98";
+                } else if (grade > 87) {
+                    element.style.backgroundColor = "#e2fb98";
+                } else {
+                    element.style.backgroundColor = "lightcoral";
+                }
+
+            }
+        } else {
+            console.log("No .percent element found in this .content element");
+        }
+    });
+ 
+}
+
+function highlightClasses() {
+
+    const gradeElements = document.querySelectorAll(".nav-class");
+
+    gradeElements.forEach(element => {
+
+        element.classList.remove("pure-table-odd")
+        element.style.backgroundColor = "lightgray";
+
+        const percentElement = element.querySelector(".percent");
+
+        if (percentElement) {
+            const grade = extractNumber(percentElement.textContent);
+            if (grade !== null) {
+
+                if (grade > 93) {
+                    element.style.backgroundColor = "#98FB98";
+                } else if (grade > 87) {
+                    element.style.backgroundColor = "#e2fb98";
+                } else {
+                    element.style.backgroundColor = "lightcoral";
+                }
+
+            }
+        } else {
+            console.log("No .percent element found in this .content element");
+        }
+    });
+
+}
+
+const path = window.location.pathname;
+
+if (path === "/home") {
+    console.log("home rn");
+    highlightOverview();
+} else if (path === "/home/schedule") {
+    console.log("rows rn");
+    highlightClasses();
+} else {
+    console.log("No specific function for this path.");
+}
